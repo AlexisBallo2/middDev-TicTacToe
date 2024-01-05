a39a840d0b6ff2ebd7e10c937dac5d552dbb1121
AlexisBallo2 <alexispballo@gmail.com>
2024-01-04 20:21:05 -0500

    intro page

---
+++ app/intro/page.js
@@ -0,0 +1,36 @@
+// ignore from here
+"use client"
+
+export default async function intro() {
+	console.log("running your code:")
+// to here
+
+
+
+
+
+
+
+
+
+	// here we will learn the basics of JavaScript
+	let x = 10
+	console.log(x)
+
+
+
+
+
+
+
+
+
+
+	// ignore from here
+	console.log("finished running!")
+
+	return (
+		<>See the console for details</>
+	)
+}
+// to here
