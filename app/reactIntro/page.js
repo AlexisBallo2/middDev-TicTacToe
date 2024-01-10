9663364e554f641c1d859af7732a33bdf6e2d945
AlexisBallo2 <alexispballo@gmail.com>
2024-01-10 15:35:18 -0500

    reactIntro

---
+++ app/reactIntro/page.js
@@ -0,0 +1,22 @@
+"use client"
+
+
+// example component:
+function C1() {
+	return (
+		<div>
+			Testing
+		</div>
+	)
+}
+
+
+export default function reactIntro() {
+
+	return (
+		<div>
+			Hello
+			<C1 />
+		</div>
+	)
+}
